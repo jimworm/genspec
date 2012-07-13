2dfd3ad3ebfe7e4029dc1889ccebf47723da15cf
Vito Botta <vito@botta.name>
2012-07-13 22:30:36 +0300

    Renamed matchers "generate" and "delete" to "generate_file" and "delete_file" since these conflict with FactoryGirl's "generate" method and the "delete" method used in controller specs to perform requests with the delete HTTP verb.

--- spec/generators/test_rails3_spec.rb
+++ spec/generators/test_rails3_spec.rb
@@ -7,7 +7,7 @@ describe :test_rails3 do
   
   it "should modify Gemfile" do
     out = ""
-    subject.should generate {
+    subject.should generate_file {
       File.read("Gemfile").strip.should_not be_blank
       out.concat File.read("Gemfile")
     }
@@ -16,8 +16,8 @@ describe :test_rails3 do
   
   context "with no options or arguments" do
     it "should generate a file called default_file" do
-      subject.should     generate("default_file")
-      subject.should_not generate("some_other_file")
+      subject.should     generate_file("default_file")
+      subject.should_not generate_file("some_other_file")
       
       subject.should     call_action(:create_file)
       subject.should     call_action(:create_file, "default_file")
@@ -29,14 +29,14 @@ describe :test_rails3 do
     end
     
     it "should generate a file with specific content" do
-      subject.should generate("default_file") { |content| content.should == "content!" }
-      subject.should generate("default_file") { |content| content.should_not == "!content" }
-      subject.should_not generate("some_other_file")
+      subject.should generate_file("default_file") { |content| content.should == "content!" }
+      subject.should generate_file("default_file") { |content| content.should_not == "!content" }
+      subject.should_not generate_file("some_other_file")
     end
     
     it "should generate a template called 'default_template'" do
-      subject.should generate(:template)
-      subject.should generate(:template, 'file', 'file_template')
+      subject.should generate_file(:template)
+      subject.should generate_file(:template, 'file', 'file_template')
     end
     
     it "should output 'create    file'" do
@@ -44,10 +44,10 @@ describe :test_rails3 do
     end
     
     it "shoud generate a directory called 'a_directory'" do
-      subject.should     generate(:empty_directory)
-      subject.should     generate(:empty_directory, "a_directory")
-      subject.should     generate("a_directory")
-      subject.should_not generate(:empty_directory, 'another_directory')
+      subject.should     generate_file(:empty_directory)
+      subject.should     generate_file(:empty_directory, "a_directory")
+      subject.should     generate_file("a_directory")
+      subject.should_not generate_file(:empty_directory, 'another_directory')
       subject.should     empty_directory("a_directory")
       subject.should_not empty_directory("another_directory")
     end
@@ -76,23 +76,23 @@ describe :test_rails3 do
     with_args :test_arg
     
     it "should generate file 'test_arg'" do
-      subject.should generate('test_arg')
+      subject.should generate_file('test_arg')
     end
   end
 
   with_args :test_arg do
     it "should generate file 'test_arg'" do
-      subject.should generate('test_arg')
+      subject.should generate_file('test_arg')
     end
     
     it "should not generate template_name" do
       # because that option hasn't been given in this context.
-      subject.should_not generate('template_name')
+      subject.should_not generate_file('template_name')
     end
     
     with_generator_options :behavior => :revoke do
       it "should delete file 'test_arg'" do
-        subject.should generate {
+        subject.should generate_file {
           File.should_not exist("test_arg")
         }
       end
@@ -100,18 +100,18 @@ describe :test_rails3 do
       # demonstrate use of the `delete` matcher, which is equivalent to
       # above:
       it "should destroy file 'test_arg'" do
-        subject.should delete("test_arg")
+        subject.should delete_file("test_arg")
       end
     end
     
     # ...and a test of nested args
     with_args "template_name" do
       it "should generate file 'test_arg'" do
-        subject.should generate('test_arg')
+        subject.should generate_file('test_arg')
       end
 
       it "should generate file 'template_name'" do
-        subject.should generate("template_name")
+        subject.should generate_file("template_name")
       end
     end
   end
